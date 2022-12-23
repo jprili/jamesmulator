fd6b1ae7b3610b0ff0b3dc707fb931640e9d7ae6
jprili <rilijames@gmail.com>
2022-12-23 13:31:52 -0800

    added logic and ignore bot metadata

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include <fstream>
 #include <dpp/dpp.h>
 #include <json/json.h>
+#include "bot.cpp"
 
 int main() {
+    Bot botInfo;
+    dpp::cluster bot(botInfo.getToken());
 
     return 0;
 }
