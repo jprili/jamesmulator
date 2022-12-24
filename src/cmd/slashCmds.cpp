d91cd4eb7758b149ee381365a71e75d8cfa8b051
jprili <rilijames@gmail.com>
2022-12-23 19:55:37 -0800

    added rounding to ping

--- src/cmd/slashCmds.cpp
+++ src/cmd/slashCmds.cpp
@@ -9,12 +9,12 @@
 void ping(dpp::cluster& bot, const dpp::slashcommand_t& event) {
     // initialise values to send
     const double wsPing = bot.get_shard(0)->websocket_ping;
-    const double totalPing = (bot.rest_ping + wsPing) * 1000;
+    const int totalPing = std::round((bot.rest_ping + wsPing) * 1000);
 
-    const std::string msg = "It took " + std::to_string(totalPing) + "to respond!";
+    const std::string msg = "It took " + std::to_string(totalPing) + " ms to respond!";
 
     // reply with msg
-    event.reply(dpp::message().set_flags(dpp::m_ephemeral).set_content(msg));
+    event.reply(dpp::message().set_content(msg));
 }
 
 void joke(dpp::cluster& bot, const dpp::slashcommand_t& event) {
